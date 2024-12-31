7be17ada1cee965c2932dddd7007853082700d7f
Riya <riddhidapkosh@gmail.com>
2024-12-31 12:14:53 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "Riddhi"
+ 
+// console.log(typeof(score));
+// console.log(typeof score);
+
+let valueInNumber = Number (score)
+// console.log( typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1; false =>0
+
+let isLoggedIn = ""
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanisLoggedIn);
+
+// 1 => true ; 0=> false
+// "" => false
+//"Riddhi" => true
+
+let someNumber=33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
